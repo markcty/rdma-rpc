28a97758ac0a9e0c2ff8da81b885150361e30e0e
wangtx <wang.tianxia@foxmail.com>
2022-10-10 14:01:30 +0800

    update comments

--- rdma-rpc/src/server_stub.rs
+++ rdma-rpc/src/server_stub.rs
@@ -2,6 +2,9 @@ use alloc::string::String;
 
 /// Server stub for RPC call
 pub trait ServerStub {
+    /// Register rpc handler, bound with a rpc_id
     fn register_rpc_handler(rpc_id: u64) -> Result<(), ()>;
+
+    /// Start an event loop, wait for requests
     fn run_event_loop() -> Result<(), ()>;
 }
