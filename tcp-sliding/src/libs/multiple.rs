1d7465a1126e1f414f68a6c1487fe4b147a32e99
huchao.66 <huchao.66@bytedance.com>
2022-12-04 21:15:59 +0800

    run with tcp

---
+++ tcp-sliding/src/libs/multiple.rs
@@ -0,0 +1,3 @@
+pub fn multiple_test() {
+    println!("hello world");
+}
--- tcp-sliding/src/main.rs
+++ tcp-sliding/src/main.rs
@@ -1,3 +1,13 @@
+mod libs;
+mod tcp;
+use std::thread;
+use std::time;
+use tcp::client;
+use tcp::server;
+
 fn main() {
-    println!("hello world")
+    println!("hello world");
+    server::run_server_background();
+    thread::sleep(time::Duration::from_secs(1));
+    client::run_client();
 }
